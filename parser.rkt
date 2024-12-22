1eaa27ed0189e5d1ac79c031abb1dbfb023dbcb0
aa57c <aa57c@umsystem.edu>
2024-12-22 15:47:04 -0600

    added error handling and comments

--- parser.rkt
+++ parser.rkt
@@ -3,269 +3,138 @@
 (require parser-tools/lex)
 (require (prefix-in : parser-tools/lex-sre))
 
-
-;scanner to be called on input files
-;derived from this source: https://matt.might.net/articles/lexers-in-racket/
+; Lexer: Scanner for input files
 (define calc-lexer
   (lexer
-   ;key words write and read
-   ["write"
-    (cons `WRITE
-          (calc-lexer input-port))]
-   ["read"
-    (cons `READ
-          (calc-lexer input-port))]
-   ;reads in each character if present until whitespace is found, creates an ID token
-   [(:+ (:or (char-range #\a #\z)(char-range #\A #\Z)))
-    (cons `ID
-          (calc-lexer input-port))]
-   ;token for assignment operator
-   [(:: (:? #\:) (:+ #\=))
-    (cons `ASS_OP
-          (calc-lexer input-port))]
-   ;token for both multiplication and division operator
-   [(:or #\* #\/)
-    (cons `MULT_OP
-          (calc-lexer input-port))]
-   ;end token
-   [(:+ (:or #\$ #\$))
-    (cons `END
-          (calc-lexer input-port))]
-   ;left parenthesis token
-   [#\(
-    (cons `LPAR
-          (calc-lexer input-port))]
-   ;right parenthesis token
-   [#\)
-    (cons `RPAR
-          (calc-lexer input-port))]
-   ;integer/number token
-   [(:: (:? #\-)(:+ (char-range #\0 #\9)))
-        (cons `INT
-              (calc-lexer input-port))]
-   ;subtraction/addition token
-   [(:or #\+ #\-)
-    (cons `ADD_OP
-          (calc-lexer input-port))]
-   ;white spaces are ignored
-   [whitespace
-    (calc-lexer input-port)]
-   [(eof)
-    '()]))
+   ["write" (cons `WRITE (calc-lexer input-port))]  ; Match the keyword "write"
+   ["read" (cons `READ (calc-lexer input-port))]    ; Match the keyword "read"
+   [(:+ (:or (char-range #\a #\z) (char-range #\A #\Z)))  ; Match identifiers (letters a-z, A-Z)
+    (cons `ID (calc-lexer input-port))]
+   [(:: (:? #\:) (:+ #\=)) (cons `ASS_OP (calc-lexer input-port))]  ; Match assignment operator ":="
+   [(:or #\* #\/) (cons `MULT_OP (calc-lexer input-port))]  ; Match multiplication operator "*" or "/"
+   [(:+ (:or #\$)) (cons `END (calc-lexer input-port))]  ; Match the end of the input "$"
+   [#\( (cons `LPAR (calc-lexer input-port))]  ; Match opening parenthesis "("
+   [#\) (cons `RPAR (calc-lexer input-port))]  ; Match closing parenthesis ")"
+   [(:: (:? #\-) (:+ (char-range #\0 #\9))) (cons `INT (calc-lexer input-port))]  ; Match integers
+   [(:or #\+ #\-) (cons `ADD_OP (calc-lexer input-port))]  ; Match addition operator "+" or "-"
+   [whitespace (calc-lexer input-port)]  ; Ignore whitespace
+   [(eof) '()]  ; End of file (EOF) condition
+))
 
-
-
-#|
-all functions beyond this point were derived from the class textbook (pg 74-76)
-|#
-
-;match function to check if the token is valid, if end token is found, program ends for specific input file
-;otherwise throw a message saying there was a syntax error
-(define (match expected token_list)
+; Match function for token validation
+(define (match expected token-list)
   (cond
-    [(equal? expected 'END) (displayln "valid tokens")]
-    [(equal? expected (first token_list)) (rest token_list)]
-    [else displayln "failed. syntax error found"]))
+    [(null? token-list) (error 'match (format "Unexpected end of tokens, expected: ~a" expected))]  ; If list is empty, throw an error
+    [(equal? expected (first token-list)) (rest token-list)]  ; If the expected token matches, return the rest of the tokens
+    [else (error 'match (format "Syntax error: expected ~a but found ~a" expected (first token-list)))]))  ; Syntax error if unexpected token is found
 
-
-
-;parse function to start program
-(define (parse token_list)
-  ;checks to see if the first token is an ID, read, or write key word
+; Addition operator grammar
+(define (add-op token-list)
   (cond
-    [(or (equal? (first token_list) 'ID)
-         (equal? (first token_list) 'READ)
-         (equal? (first token_list) 'WRITE))
-     ;if so then call statement list grammar, then recursively call parse again 
-     (parse(stmt_list token_list))]
-    ;handles the end token towards the end of the program
-    [(equal? (first token_list) 'END) (match 'END token_list)]
-    [else displayln "failed. syntax error found"]))
-;statement list grammar
-(define (stmt_list token_list)
+    [(equal? (first token-list) 'ADD_OP) (match 'ADD_OP token-list)]  ; Match addition operator
+    [else (error 'add-op "Syntax error in addition operator")]))  ; Error if not an addition operator
+
+; Multiplication operator grammar
+(define (mult-op token-list)
   (cond
-    [(or (equal? (first token_list) 'ID)
-         (equal? (first token_list) 'READ)
-         (equal? (first token_list) 'WRITE))
-     (stmt_list(stmt token_list))]
-    [(equal? (first token_list) 'END) token_list]
-    [else displayln "failed. syntax error found"]))
-;statment grammar
-(define (stmt token_list)
+    [(equal? (first token-list) 'MULT_OP) (match 'MULT_OP token-list)]  ; Match multiplication operator
+    [else (error 'mult-op "Syntax error in multiplication operator")]))  ; Error if not a multiplication operator
+
+; Factor grammar
+(define (factor token-list)
   (cond
-    #|if first token is ID, first match the ID token and remove from the list,
-    then assume assignment operator is next then match that. finally, call expression function|#
-    [(equal? (first token_list) 'ID) (express(match 'ASS_OP(match 'ID token_list)))]
-    #|if first token is equal to read, match the read and id token after it, remove both from the list|#
-    [(equal? (first token_list) 'READ) (match 'ID(match 'READ token_list))]
-    #|if first token is equal to write, match the write token and call expression function|#
-    [(equal? (first token_list) 'WRITE) (express(match 'WRITE token_list))]
-    #|if syntax error was found at this point, throw that message into the console|#
-    [else displayln "failed. syntax error found"]))
-;expression grammar
-(define (express token_list)
-  ;checks to to see if first token is an id, an int, or a left parenthesis
+    [(equal? (first token-list) 'ID) (match 'ID token-list)]  ; Match an identifier (ID)
+    [(equal? (first token-list) 'INT) (match 'INT token-list)]  ; Match an integer
+    [(equal? (first token-list) 'LPAR)  ; Match opening parenthesis and process the expression inside
+     (match 'RPAR (express (match 'LPAR token-list)))]
+    [else (error 'factor "Syntax error in factor")]))  ; Error for invalid factor
+
+; Factor tail grammar
+(define (factor-tail token-list)
   (cond
-    [(or (equal? (first token_list) 'ID)
-         (equal? (first token_list) 'INT)
-         (equal? (first token_list) 'LPAR))
-     ;if so call term expression first, then call term_tail
-     (term_tail(term token_list))]
-    ;if syntax error was found at this point, throw that message into console
-    [else displayln "failed. syntax error found"]))
+    [(member (first token-list) '(ADD_OP RPAR ID READ WRITE END)) token-list]  ; If token is one of these, we can return the token list as is
+    [(equal? (first token-list) 'MULT_OP)  ; Match multiplication operator and continue processing
+     (factor-tail (factor (mult-op token-list)))]
+    [else (error 'factor-tail
+                 (format "Syntax error in factor tail: unexpected token ~a, remaining tokens: ~a"
+                         (first token-list) token-list))]))  ; Error if unexpected token in factor tail
 
-;term tail grammar
-(define (term_tail token_list)
+; Term grammar
+(define (term token-list)
   (cond
-    ;if it's any of these...
-    [(or (equal? (first token_list) 'RPAR)
-         (equal? (first token_list) 'ID)
-         (equal? (first token_list) 'READ)
-         (equal? (first token_list) 'WRITE)
-         (equal? (first token_list) 'END))
-     ;skip (return the same list, these will later be matched and removed in other functions in this program)
-     token_list]
-    ;if the first token is an add or subtraction sign, call the addition operator function then term, then term_tail
-    [(equal? (first token_list) 'ADD_OP) (term_tail(term(add_op token_list)))]
-    ;if syntax error was found at this point, throw that message into console
-    [else displayln "failed. syntax error found"]))
-;term grammar
-(define (term token_list)
-  ;if the first token is any of these...
+    [(member (first token-list) '(ID INT LPAR))  ; If token is an ID, INT, or LPAR, process as a factor
+     (factor-tail (factor token-list))]
+    [else (error 'term
+                 (format "Syntax error in term: expected ID, INT, or LPAR but found ~a, remaining tokens: ~a"
+                         (first token-list) token-list))]))  ; Error if invalid term
+
+; Term tail grammar
+(define (term-tail token-list)
   (cond
-    [(or (equal? (first token_list) 'ID)
-         (equal? (first token_list) 'INT)
-         (equal? (first token_list) 'LPAR))
-     ;then call factor, then factor tail
-     (factor_tail(factor token_list))]
-    ;if syntax error was found at this point, throw that message into console
-    [else displayln "failed. syntax error found"]))
-;factor tail grammar
-(define (factor_tail token_list)
-  ;if the first token are any of these...
+    [(member (first token-list) '(RPAR ID READ WRITE END)) token-list]  ; If token is one of these, we can return the token list
+    [(equal? (first token-list) 'ADD_OP)  ; Match addition operator and continue processing
+     (term-tail (term (add-op token-list)))]
+    [else (error 'term-tail
+                 (format "Syntax error in term tail: unexpected token ~a, remaining tokens: ~a"
+                         (first token-list) token-list))]))  ; Error if unexpected token in term tail
+
+; Expression grammar
+(define (express token-list)
   (cond
-    [(or (equal? (first token_list) 'ADD_OP)
-         (equal? (first token_list) 'RPAR)
-         (equal? (first token_list) 'ID)
-         (equal? (first token_list) 'READ)
-         (equal? (first token_list) 'WRITE)
-         (equal? (first token_list) 'END))
-     ;skip (return the same list, these will later be matched and removed in other functions in this program)
-     token_list]
-    ;if syntax error was found at this point, throw that message into console
-    [(equal? (first token_list) 'MULT_OP) (factor_tail(factor(mult_op token_list)))]
-    [else displayln "failed. syntax error found"]))
-;factor grammar
-(define (factor token_list)
+    [(member (first token-list) '(ID INT LPAR))  ; If token is ID, INT, or LPAR, process as a term
+     (term-tail (term token-list))]
+    [else (error 'express
+                 (format "Syntax error in expression: unexpected token ~a, remaining: ~a"
+                         (first token-list) token-list))]))  ; Error if unexpected token in expression
+
+; Statement grammar
+(define (stmt token-list)
   (cond
-    [(equal? (first token_list) 'ID) (match 'ID token_list)]
-    [(equal? (first token_list) 'INT) (match 'INT token_list)]
-    [(equal? (first token_list) 'LPAR) (match 'RPAR(express(match 'LPAR token_list)))]
-    ;if syntax error was found at this point, throw that message into console
-    [else displayln "failed. syntax error found"]))
+    [(equal? (first token-list) 'ID)  ; If token is ID, match assignment operation
+     (express (match 'ASS_OP (match 'ID token-list)))]
+    [(equal? (first token-list) 'READ)  ; If token is READ, match ID
+     (match 'ID (match 'READ token-list))]
+    [(equal? (first token-list) 'WRITE)  ; If token is WRITE, match expression
+     (express (match 'WRITE token-list))]
+    [else (error 'stmt
+                 (format "Syntax error in statement: unexpected token ~a, remaining: ~a"
+                         (first token-list) token-list))]))  ; Error if unexpected token in statement
 
-(define (add_op token_list)
+; Statement list grammar
+(define (stmt-list token-list)
   (cond
-    [(equal? (first token_list) 'ADD_OP) (match 'ADD_OP token_list)]
-    ;if syntax error was found at this point, throw that message into console
-    [else displayln "failed. syntax error found"]))
+    [(member (first token-list) '(ID READ WRITE))  ; Process multiple statements
+     (stmt-list (stmt token-list))]
+    [(equal? (first token-list) 'END) token-list]  ; End of statement list
+    [else (error 'stmt-list
+                 (format "Syntax error in statement list: unexpected token ~a, remaining: ~a"
+                         (first token-list) token-list))]))  ; Error if unexpected token in statement list
 
-(define (mult_op token_list)
+; Parse function (starting point)
+(define (parse token-list)
   (cond
-    [(equal? (first token_list) 'MULT_OP) (match 'MULT_OP token_list)]
-    ;if syntax error was found at this point, throw that message into console
-    [else displayln "failed. syntax error found"]))
+    [(null? token-list) (error 'parse "Empty token list")]  ; Error if token list is empty
+    [(member (first token-list) '(ID READ WRITE))  ; If token is ID, READ, or WRITE, start processing statement list
+     (parse (stmt-list token-list))]
+    [(equal? (first token-list) 'END) (match 'END token-list)]  ; If token is END, match the end of the input
+    [else (error 'parse
+                 (format "Unexpected token: ~a, remaining tokens: ~a"
+                         (first token-list) token-list))]))  ; Error for unexpected token
 
+; Wrapper to process multiple files
+(define (process-files file-names)
+  (for-each
+   (lambda (file-name)
+     (displayln (format "Processing file: ~a" file-name))  ; Print file being processed
+     (with-handlers
+         ([exn:fail? (lambda (exn)
+                       (displayln (format "Error in file ~a: ~a" file-name (exn-message exn)))
+                       (displayln "Skipping to the next file..."))])  ; Handle any exceptions (e.g., file read errors)
+       (let ([token-list (calc-lexer (open-input-file file-name))])  ; Tokenize the file
+         (displayln (format "Token list: ~a" token-list)) ; Optional debugging: print token list
+         (parse token-list)  ; Parse the token list
+         (displayln "Parsing completed successfully"))))  ; Indicate successful parsing
+   file-names))
 
-#|
-Note for Mr. Hare,
-Sorry for the inconvenice. I ran out of time trying to decide how to read in all the files at once without calling them like this below.
-I have tried looping and recursive calling. But I had some sort of scope issue where open-input-file was not working within the functions that
-I have wrote above. It didn't like the parameter I passed into it (the file string) for some reason. Even though it was a string. But this was the best
-I could come up with. Debug really shows how each input goes through the scanner and parser. However, my parser works as well as my scanner, which hopefully
-is something you can work with.
-
-I found this project really challenging. I have never done anything like this before. But I am glad I did because now I have a pretty good idea about how a part
-of the compiler works. 
-|#
-
-
-
-(define token_list(calc-lexer(open-input-file "input01.txt")))
-(parse token_list)
-(define token_list2(calc-lexer(open-input-file "input02.txt")))
-(parse token_list2)
-(define token_list3(calc-lexer(open-input-file "input03.txt")))
-(parse token_list3)
-(define token_list4(calc-lexer(open-input-file "input04.txt")))
-(parse token_list4)
-(define token_list5(calc-lexer(open-input-file "input05.txt")))
-(parse token_list5)
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
-  
-
-  
-    
-    
-    
-    
-    
-   
-    
-
-    
-    
-  
-
-   
-  
-
-    
-
-    
-
-
-    
-
-    
-    
-    
-     
-     
-   
-
-
-
-
-
-
-       
-
-
-
-
-
-               
-   
-  
-
-
-
-
-
-
-
-
+; Entry point for file processing
+(process-files '("input01.txt" "input02.txt" "input03.txt" "input04.txt" "input05.txt"))  ; Process the specified input files
